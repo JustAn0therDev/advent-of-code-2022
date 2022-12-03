831d8b79f00e9cdd056bbfc6333814ea46b2107f
Ruan Montelo <ruanmontelo@gmail.com>
2022-12-03 11:57:31 -0300

    Finished day two

--- Advent Of Code 2022/DayTwo.cs
+++ Advent Of Code 2022/DayTwo.cs
@@ -10,10 +10,10 @@ public class DayTwo : IProblem
 	public void SolveAllAndPrint()
 	{
 		Console.WriteLine($"Part one: {SolvePartOne()}");
-		// Console.WriteLine($"Part two: {SolvePartTwo()}");
+		Console.WriteLine($"Part two: {SolvePartTwo()}");
 	}
 
-	private int CalculateMatch(string match)
+	private int CalculateMatchPartOne(string match)
 	{
 		// A, B, C -> Rock, Paper, Scissors
 		// X, Y, Z -> Rock, Paper, Scissors
@@ -54,6 +54,66 @@ public class DayTwo : IProblem
 		return matchTotal;
 	}
 
+	private int CalculateMatchPartTwo(string match)
+	{
+		// A, B, C -> Rock, Paper, Scissors
+		// X, Y, Z -> Lose, Draw, Win
+		int matchTotal = 0;
+
+		string[] matchComponents = match.Replace("\n", "").Split(" ");
+
+		if (matchComponents[1] == "Z")
+		{
+			matchTotal += 6;
+
+			switch (matchComponents[0])
+			{
+				case "A":
+					matchTotal += 2;
+					break;
+				case "B":
+					matchTotal += 3;
+					break;
+				case "C":
+					matchTotal += 1;
+					break;
+			}
+		}
+		else if (matchComponents[1] == "Y")
+		{
+			matchTotal += 3;
+
+			switch (matchComponents[0])
+			{
+				case "A":
+					matchTotal += 1;
+					break;
+				case "B":
+					matchTotal += 2;
+					break;
+				case "C":
+					matchTotal += 3;
+					break;
+			}
+		}
+		else {
+			switch (matchComponents[0])
+			{
+				case "A":
+					matchTotal += 3;
+					break;
+				case "B":
+					matchTotal += 1;
+					break;
+				case "C":
+					matchTotal += 2;
+					break;
+			}
+		}
+
+		return matchTotal;
+	}
+
 	public int SolvePartOne()
 	{
 		// What would your total score be if everything goes exactly according to your strategy guide?
@@ -63,13 +123,29 @@ public class DayTwo : IProblem
 
 		foreach (string match in matches)
 		{
-			total += CalculateMatch(match);
+			total += CalculateMatchPartOne(match);
 		}
 
 		return total;
 	}
 
-	// public int SolvePartTwo() {
+	public int SolvePartTwo()
+	{
+		/*
+			The second column says how the round needs to end: 
+			X means you need to lose, 
+			Y means you need to end the round in a draw, 
+			and Z means you need to win.
+		*/
+		int total = 0;
 
-	// }
+		string[] matches = _input.Split("\r");
+
+		foreach (string match in matches)
+		{
+			total += CalculateMatchPartTwo(match);
+		}
+
+		return total;
+	}
 }
