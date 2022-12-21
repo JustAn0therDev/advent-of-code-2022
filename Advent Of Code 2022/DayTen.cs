de911edaeab3318385c1bdc094973dacf7adbbed
Ruan Montelo <ruanmontelo@gmail.com>
2022-12-21 18:46:20 -0300

    Finished day ten part two

--- Advent Of Code 2022/DayTen.cs
+++ Advent Of Code 2022/DayTen.cs
@@ -1,3 +1,4 @@
+using System.Text;
 
 public class DayTen : IProblem
 {
@@ -11,7 +12,7 @@ public class DayTen : IProblem
 	public void SolveAllAndPrint()
 	{
 		Console.WriteLine($"Part one: {SolvePartOne()}");
-		// Console.WriteLine($"Part two: {SolvePartTwo()}");
+		Console.WriteLine($"Part two: \n{SolvePartTwo()}");
 	}
 
 	public int SolvePartOne()
@@ -29,6 +30,7 @@ public class DayTen : IProblem
 		while (index < _input.Length - 1)
 		{
 			cycle++;
+
 			if (cycle == 20 || cycle - lastCycle == 40)
 			{
 				lastCycle = cycle;
@@ -57,9 +59,71 @@ public class DayTen : IProblem
 		return result;
 	}
 
-	/*
-	   public int SolvePartTwo()
-	   {
-	   }
-	*/
+	public string SolvePartTwo()
+	{
+		// the X register keeps the position of a sprite in it.
+		// a sprite is the sum of three pixels and the middle of a sprite
+		// is the position (so a ...###... sprite is in position 4 in a 0-based index.)
+		// the CRT will only draw a pixel if the register has a sprite in it
+		// so if the register has "1" in it, it means that the pixel "###......." is there.
+		// if it changes value after that, two cycles have passed, so it becomes: "##......"
+
+		StringBuilder crtScreen = new();
+		StringBuilder section = new();
+		int cycle = 0;
+		int lastCycle = 0;
+		bool hasInstructionPending = false;
+		int vRegister = 1;
+		int index = 0;
+		int sectionIndex = 0;
+
+		while (index < _input.Length - 1)
+		{
+			if (vRegister == sectionIndex - 1 || vRegister == sectionIndex || vRegister == sectionIndex + 1)
+			{
+				section.Append('#');
+			}
+			else
+			{
+				section.Append('.');
+			}
+
+			cycle++;
+			sectionIndex++;
+
+			string instruction = _input[index];
+
+			if (instruction.StartsWith("addx") && !hasInstructionPending)
+			{
+				hasInstructionPending = true;
+			}
+			else if (instruction.StartsWith("addx") && hasInstructionPending)
+			{
+				hasInstructionPending = false;
+				vRegister += int.Parse(instruction.Split(" ")[1]);
+				index++;
+			}
+			else
+			{
+				// noop instruction
+				index++;
+			}
+
+			// I cannot waste cycles.
+			if (cycle - lastCycle == 40)
+			{
+				lastCycle = cycle;
+
+				crtScreen.Append(section);
+				crtScreen.AppendLine();
+				sectionIndex = 0;
+				section.Clear();
+			}
+		}
+
+        crtScreen.Append(section);
+        crtScreen.AppendLine();
+
+		return crtScreen.ToString();
+	}
 }
